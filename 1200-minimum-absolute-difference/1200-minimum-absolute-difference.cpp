f120082dec0606c5f98ffa54923215222c23def1
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2025-12-01 09:31:16 +0530

    Time: 14 ms (82.38%), Space: 36.7 MB (64.05%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -3,25 +3,25 @@
 
 class Solution {
 public:
-    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {  //tc=O(n logN), sc=O(n) due ans vector
+        sort(arr.begin(), arr.end()); 
 
         int minDiff = INT_MAX;
-        vector<vector<int>> result;
+        vector<vector<int>> ans;
 
         for (int i = 1; i < arr.size(); ++i) {
             int diff = arr[i] - arr[i - 1];
             if (diff < minDiff) {
                 // Found a new minimum difference
                 minDiff = diff;
-                result.clear();  
-                result.push_back({arr[i - 1], arr[i]});
+                ans.clear();  
+                ans.push_back({arr[i - 1], arr[i]});
             } else if (diff == minDiff) {
                 // Same as the current minimum, so add this pair
-                result.push_back({arr[i - 1], arr[i]});
+                ans.push_back({arr[i - 1], arr[i]});
             }
         }
 
-        return result;
+        return ans;
     }
 };
