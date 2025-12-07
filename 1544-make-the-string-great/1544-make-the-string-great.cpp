9cd3a3e4907e7f22206f8536621ae52ac9bc8bf6
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2025-12-07 13:03:10 +0530

    Time: 4 ms (4.39%), Space: 9 MB (15.74%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,25 @@
+#include <stack>
+
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> stk;
+        
+        for (char c : s) {
+            if (!stk.empty() && abs(stk.top() - c) == 32) {
+                stk.pop();  
+            } else {
+                stk.push(c);  
+            }
+        }
+        
+ 
+        string result = "";
+        while (!stk.empty()) {
+            result = stk.top() + result;  // Add top char of the stack to result
+            stk.pop();
+        }
+        
+        return result;
+    }
+};
