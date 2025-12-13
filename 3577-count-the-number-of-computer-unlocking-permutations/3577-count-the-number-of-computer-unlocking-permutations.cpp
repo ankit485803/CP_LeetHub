018e67b1489204e5a672a70655067994bc556005
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2025-12-13 11:03:57 +0530

    Time: 2 ms (39.72%), Space: 92.9 MB (36.27%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    int countPermutations(vector<int>& complexity) { //greedyAppoach
-        const int MOD = 1e9 + 7;
+    int countPermutations(vector<int>& complexity) {
         int n = complexity.size();
-
-        vector<pair<int,int>> a;
-        for (int i = 1; i < n; i++) {
-            a.push_back({complexity[i], i});
-        }
-
-        // Sort by complexity
-        sort(a.begin(), a.end());
+        const int MOD = 1e9 + 7;
 
         long long ans = 1;
-        int unlocked = 1; // computer 0 already unlocked
 
-        for (auto &[comp, idx] : a) {
-            // count how many unlocked j < idx with smaller complexity
-            int choices = 0;
-            for (int j = 0; j < idx; j++) {
-                if (complexity[j] < comp) choices++;
+        for (int i = 1; i < n; i++) {
+            // If any computer cannot be unlocked by computer 0
+            if (complexity[i] <= complexity[0]) {
+                return 0;
             }
 
-            if (choices == 0) return 0;
-
-            ans = (ans * choices) % MOD;
-            unlocked++;
+            ans = (ans * i) % MOD;  // multiply choices
         }
 
-        return ans;
+        return (int)ans;
     }
 };
