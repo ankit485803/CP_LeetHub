037eb3617140edd501748a4e03c0f44ec52625ba
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2025-12-18 10:51:20 +0530

    Time: 0 ms (100%), Space: 22.5 MB (77.33%) - LeetHub

---
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minOperations(std::vector<int>& nums, int k) {  //tc=O(n), sc=O(k)
+        int n = nums.size(); 
+
+        vector<bool> seen(k, false);  // Boolean array to track collected elements
+        int collected = 0;  
+        int ops = 0;  
+
+    
+        for (int i=n-1; i>=0; i--) {
+            ops++;
+            
+            int val = nums[i];
+            
+            // If the element is in the range [1, k] and hasn't been collected yet
+            if (val <= k && !seen[val - 1]) {
+                seen[val - 1] = true;  
+                collected++;  
+            }
+
+            if (collected == k) {
+                return ops;
+            }
+        }
+
+        return ops;  
+    }
+};
+
