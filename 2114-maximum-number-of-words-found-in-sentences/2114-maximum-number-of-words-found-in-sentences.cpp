4b5b8d5e9dac6dc74f48a6751fe0bde207595c13
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2025-12-25 07:36:35 +0530

    Time: 8 ms (5.11%), Space: 14.3 MB (5.26%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,24 @@
+#include <sstream>
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int maxWord = 0;
+        
+        for (int i = 0; i < sentences.size(); i++) {
+            string curr = sentences[i];
+            stringstream ss(curr);
+            string word;
+            int wordCount = 0;
+            
+            // Count words in the current sentence
+            while (ss >> word) {
+                wordCount++;
+            }
+
+            maxWord = max(maxWord, wordCount);
+        }
+        
+        return maxWord;
+    }
+};
+
