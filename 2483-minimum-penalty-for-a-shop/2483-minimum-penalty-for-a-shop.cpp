5ebea581ee80fecdbd284867847d1873f53dc957
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2025-12-28 07:25:13 +0530

    Time: 2 ms (86.96%), Space: 13.3 MB (78.28%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int bestClosingTime(string customers) {
+    int bestClosingTime(string customers) {  //tc=O(n), sc=O(1)
         int n = customers.size();
 
-        // Step 1: Count total Y's
+        // Step1: Count total Y's
         int penalty = 0;
         for (char c : customers) {
             if (c == 'Y') penalty++;
@@ -12,7 +12,7 @@ public:
         int minPenalty = penalty;
         int bestHour = 0;
 
-        // Step 2: Try closing at each hour j (1 to n)
+        // Step2: Try closing at each hour j (1 to n)
         for (int j = 1; j <= n; j++) {
             if (customers[j - 1] == 'Y') {
                 penalty--;   // no longer closed during a customer hour
