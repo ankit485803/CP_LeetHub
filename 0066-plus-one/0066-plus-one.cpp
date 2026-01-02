74f556b96dfb257b6301326d83b61f159e72ec2f
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-01-02 09:29:15 +0530

    Time: 0 ms (100%), Space: 11.6 MB (14.68%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        
+        //BRUTE-FORCE approach
+        for(int i=n-1; i>=0; --i) {
+            if(digits[i] < 9) {  //when less than 9, because this last one to decide the length of arr
+            
+                digits[i] += 1;  //add one
+                return digits;
+
+            }
+            digits[i] = 0; // Set current digit to 0 and carry over
+
+        }
+        digits.insert(digits.begin(), 1); //prepand 1 to arr
+        return digits;
+
+    }
+};
+
+//time complexity = O(n), space = O(1)
