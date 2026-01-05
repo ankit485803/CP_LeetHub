f9e8db45aece487bd99cbc8c4dc7a7283615ad00
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-01-05 17:25:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        long long totalSum = 0;  
+        long long minAbsVal = LONG_MAX;  
+        int negativeCount = 0; 
+        
+        for (int i = 0; i < matrix.size(); ++i) {
+            for (int j = 0; j < matrix[i].size(); ++j) {
+                int num = matrix[i][j];
+                totalSum += abs(num);
+                minAbsVal = min(minAbsVal, abs(num));
+                if (num < 0) {
+                    negativeCount++;
+                }
+            }
+        }
+        
+        // If we have an odd count of negative numbers, one negative number must remain
+        if (negativeCount % 2 == 1) {
+            totalSum -= 2 * minAbsVal;
+        }
+        
+        return totalSum;
+    }
+};
