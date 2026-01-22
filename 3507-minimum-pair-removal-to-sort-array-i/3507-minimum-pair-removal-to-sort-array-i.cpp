536a383962474c6d9d9e721c3633063d52eb2d99
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-01-22 07:34:25 +0530

    Time: 0 ms (100%), Space: 26.4 MB (81.3%) - LeetHub

---
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isNonDecreasing(const vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i - 1]) return false;
+        }
+        return true;
+    }
+
+    int minimumPairRemoval(vector<int>& nums) {
+        int ops = 0;
+
+        while (!isNonDecreasing(nums)) {
+            int minSum = INT_MAX;
+            int idx = 0;
+
+            // Find leftmost adjacent pair with minimum sum
+            for (int i = 0; i + 1 < nums.size(); i++) {
+                int s = nums[i] + nums[i + 1];
+                if (s < minSum) {
+                    minSum = s;
+                    idx = i;
+                }
+            }
+
+            // Replace the pair with their sum
+            nums[idx] = nums[idx] + nums[idx + 1];
+            nums.erase(nums.begin() + idx + 1);
+
+            ops++;
+        }
+
+        return ops;
+    }
+};
