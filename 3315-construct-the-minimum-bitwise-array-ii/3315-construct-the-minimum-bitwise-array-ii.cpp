c54ea5ad6e09bfd43e18c5725fb9c98081e8da27
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-01-22 07:15:40 +0530

    Time: 0 ms (100%), Space: 28.2 MB (83.26%) - LeetHub

--- 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
-        vector<int> ans;
-        for (int x : nums) {
-            int a = -1;
-            for (int i = 0; i <= x; ++i) {
-                if ((i | (i + 1)) == x) {
-                    a = i;
-                    break; // take the smallest
-                }
+        int n = nums.size();
+        vector<int> ans(n);
+
+        for(int i = 0; i < n; i++) {
+            int num = nums[i];
+
+            if(num == 2) { // special case
+                ans[i] = -1;
+                continue;
             }
-            ans.push_back(a);
+
+            bool isFound = false;
+
+            for(int j = 0; j < 32; j++) { // iterate bits LSB → MSB
+                if((num & (1 << j)) > 0) { // set bit → skip
+                    continue;
+                }
+                // Found first 0 bit at position j
+                int x = num ^ (1 << (j - 1)); // flip previous bit
+                ans[i] = x;
+                isFound = true;
+                break;
+            }
+
+            if(!isFound) {
+                ans[i] = -1;
+            }
         }
+
         return ans;
     }
 };
