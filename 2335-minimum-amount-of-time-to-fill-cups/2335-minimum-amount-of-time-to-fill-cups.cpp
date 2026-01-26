4a5ae2511224349980993abf9b0bbe9589ccea11
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-01-26 09:13:00 +0530

    Time: 0 ms (100%), Space: 14.2 MB (47.58%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int fillCups(vector<int>& amount) {
+    int fillCups(vector<int>& amount) {  //tc=O(1)=sc
         int sum = amount[0] + amount[1] + amount[2];
         int mx = max({amount[0], amount[1], amount[2]});
 
