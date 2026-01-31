d6767668a829c3be4741aebca75de62804bd5dc5
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-01-31 08:12:43 +0530

    Time: 0 ms (100%), Space: 19.8 MB (62.61%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,22 +1,44 @@
+
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        //optimized using BS because sortedArr given
-        int left = 0, right = letters.size()-1;
-        int result = letters[0];   //defaultSet to firstChar
+        //bruteForeApporach - O(n) tc,  sc=O(1)
+        int n = letters.size();
 
-        while(left <= right) {
-            int mid = left + (right-left)/2;
-
-            if(letters[mid] > target) {  //searchInRightHalf
-                result = letters[mid];
-                right = mid - 1;
-
-            } else {
-                left = mid + 1;  //leftHalfSearching
+        for(int i=0; i<n; i++) {
+            if(letters[i] > target) {  //checkCondition just greater using ASCII value
+                return letters[i];
             }
         }
-
-        return result;
+        //notFound in letter then return firstChar 
+        return letters[0];
     }
 };
+
+
+
+
+
+
+// class Solution {
+// public:
+//     char nextGreatestLetter(vector<char>& letters, char target) {
+//         //optimized using BS because sortedArr given
+//         int left = 0, right = letters.size()-1;
+//         int result = letters[0];   //defaultSet to firstChar
+
+//         while(left <= right) {
+//             int mid = left + (right-left)/2;
+
+//             if(letters[mid] > target) {  //searchInRightHalf
+//                 result = letters[mid];
+//                 right = mid - 1;
+
+//             } else {
+//                 left = mid + 1;  //leftHalfSearching
+//             }
+//         }
+
+//         return result;
+//     }
+// };
