888baf0d0026c3394673b6735fdbd31baa35a33a
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-02-05 09:22:44 +0530

    Time: 2 ms (70.24%), Space: 23 MB (55.5%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    //helper funct banawo
+    TreeNode* helper(vector<int>& nums, int st, int end) {
+        if(st > end) {
+            return NULL;
+        }
+
+        int mid = st + (end-st)/2;
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        root->left = helper(nums, st, mid-1);
+        root->right = helper(nums, mid+1, end);
+
+        return root;
+    }
+
+
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+
+        return helper(nums, 0, nums.size()-1);
+        
+    }
+};
+
+
+/*
+
+Time Complexity:
+O(n) — Every node is visited once and added to the tree.
+
+Space Complexity:
+O(log n) — Due to recursion stack space for a balanced BST (height ≈ log n)
+
+
+
+*/
