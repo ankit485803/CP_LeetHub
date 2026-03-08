f712f1f9e251c4b0dea8abab744e592dfaf48e1c
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-03-08 16:54:06 +0530

    Time: 0 ms (100%), Space: 12.6 MB (72.81%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+        string res = "";
+
+        //approach: clever trick called “diagonalization”, inspired by Cantor’s diagonal argument.
+
+        for (int i = 0; i < n; ++i) {
+            res += (nums[i][i] == '0' ? '1' : '0');
+        }
+        return res;
+    }
+};
