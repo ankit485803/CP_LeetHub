693b6736697629dee92ec597ae3ec7e4eed4279b
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-04-08 15:19:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.cpp
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        const int MOD = 1e9 + 7; // modulus to avoid overflow during multiplication
+    int M = 1e9 + 7;
 
-        // Process each query one by one
-        for (auto &q : queries) {
-            int li = q[0]; // left index of range
-            int ri = q[1]; // right index of range
-            int ki = q[2]; // step size
-            int vi = q[3]; // multiplier value
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) { //tc=O(q * n) because smallConstaint 1000
+    
+        for(auto &query : queries) { //O(query)
+            int l = query[0];
+            int r = query[1];
+            int k = query[2];
+            int v = query[3];
 
-            // Update every ki-th index in range [li, ri]
-            for (int idx = li; idx <= ri; idx += ki) {
-                // Use 1LL to promote to long long before multiplication
-                nums[idx] = (1LL * nums[idx] * vi) % MOD;
+            while(l <= r) {  //O(n)
+                nums[l] = (1LL * nums[l] * v) % M;
+                l += k;
             }
         }
 
-        // After applying all queries, calculate XOR of entire array
         int result = 0;
-        for (int num : nums) {
+        for(int &num : nums) {
             result ^= num;
         }
 
         return result;
     }
 };
-
-
-
-// TC=O(n * query), SC=O(1) 
