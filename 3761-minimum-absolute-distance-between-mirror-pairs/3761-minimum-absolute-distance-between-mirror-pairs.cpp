739f3317a4a4ae13c1c5c32fc10af7cec0e4c715
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-04-17 17:00:48 +0530

    Time: 87 ms (98.04%), Space: 131.8 MB (90.69%) - LeetHub

---
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int reverseNum(int x) {
+        int rev = 0;
+        while (x > 0) {
+            rev = rev * 10 + (x % 10);
+            x /= 10;
+        }
+        return rev;
+    }
+
+    int minMirrorPairDistance(vector<int>& nums) {
+        unordered_map<int, int> mp; // reversed value -> index
+        int ans = INT_MAX;
+
+        for (int j = 0; j < nums.size(); j++) {
+            // Check if current number matches any previous reversed
+            if (mp.find(nums[j]) != mp.end()) {
+                ans = min(ans, j - mp[nums[j]]);
+            }
+
+            // Store reverse of current number
+            int rev = reverseNum(nums[j]);
+            mp[rev] = j;
+        }
+
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
