98b187f6b74b75ffec05b2357fd8125e91bea627
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-04-23 10:58:02 +0530

    Time: 103 ms (28.75%), Space: 122.3 MB (34.75%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    typedef long long ll;
+    vector<ll> distance(vector<int>& nums) {
+        int n = nums.size();
+        vector<ll> arr(n, 0);
+
+        unordered_map<int, ll> idxSum;  //nums[i] -> sum of 1's
+        unordered_map<int, ll> idxCount;  //nums[i] -> freq
+
+        //left to right LHS  -O(N) tc
+        for(int i = 0; i < n; i++) {
+            ll freq = idxCount[nums[i]];
+            ll sum = idxSum[nums[i]];
+
+            //yahi formula banaye the
+            arr[i] += freq * i - sum;
+
+            idxCount[nums[i]] += 1;
+            idxSum[nums[i]] += i;
+        }
+
+        //pahle clearMap
+        idxSum.clear();
+        idxCount.clear();
+
+        //righToLeft  RHS
+        for(int i = n-1; i >= 0; i--) {
+            ll freq = idxCount[nums[i]];
+            ll sum = idxSum[nums[i]];
+
+            //formula
+            arr[i] += sum - freq * i;
+
+            idxCount[nums[i]] += 1;
+            idxSum[nums[i]] += i;
+        }
+
+        return arr;
+    }
+};
