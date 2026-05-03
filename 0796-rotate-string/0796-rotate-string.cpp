447a592849e8b9420a4b97487e19738e932fb0a4
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-05-03 20:12:45 +0530

    Time: 0 ms (100%), Space: 8.1 MB (84.87%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    bool rotateString(string s, string goal) {
-        int m = s.length();
+    bool rotateString(string s, string goal) {  //tc=O(n^2) nestedWork, sc=O(1)
+        int m = s.length(); 
         int n = goal.length();
 
         if(m != n) {
@@ -10,8 +10,8 @@ public:
 
         //bruteForceally check all possible rotation
         for(int rotationCount = 1; rotationCount <= m; rotationCount++) {
-            //using STL c++
-            rotate(s.begin(), s.begin() + 2, s.end());
+            //using STL c++ 
+            rotate(s.begin(), s.begin() + 1, s.end());
 
             if(s == goal) {
                 return true;
