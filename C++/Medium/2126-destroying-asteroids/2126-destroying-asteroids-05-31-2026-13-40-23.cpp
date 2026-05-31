fdea1018563e4159a0aa03309240eea93de22082
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-05-31 13:40:24 +0530

    Time: 35 ms (46.71%), Space: 106.6 MB (81.61%) - LeetHub

---
+++ C++/Medium/2126-destroying-asteroids/2126-destroying-asteroids-05-31-2026-13-40-23.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {   //tc=O(n), sc=O(1)
+        sort(begin(asteroids), end(asteroids)); 
+
+        for(int &m : asteroids) {
+            if(mass < m) {
+                return false;
+            } else if(mass >= 1e5) {
+                return true;
+            }
+
+            mass += m;
+        }
+
+        return true;
+    }
+};
