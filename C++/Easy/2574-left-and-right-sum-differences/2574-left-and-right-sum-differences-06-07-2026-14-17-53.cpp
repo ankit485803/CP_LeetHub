6f6b341e280499c54548c9743dcfaced3c33448f
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-06-07 14:17:55 +0530

    Time: 23 ms (8.73%), Space: 15 MB (81.43%) - LeetHub

---
+++ C++/Easy/2574-left-and-right-sum-differences/2574-left-and-right-sum-differences-06-07-2026-14-17-53.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {  //tc=O(n)=sc bruteForce
+        int n = nums.size();
+        vector<int> ans(n);  //take arr with n sized
+
+        for(int i=0; i<n; i++) {
+            int leftSum = 0, rightSum = 0;
+
+            //leftSide
+            for(int j=0; j < i; j++) {
+                leftSum += nums[j];
+            }
+
+            //rightSide
+            for(int j = i+1; j<n; j++) {
+                rightSum += nums[j];
+            }
+
+            ans[i] = abs(leftSum - rightSum);
+        }
+
+        return ans;
+
+    }
+};
