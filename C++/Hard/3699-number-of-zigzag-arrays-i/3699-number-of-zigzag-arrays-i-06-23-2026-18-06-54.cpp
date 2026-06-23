25d930d72759162d3d0fc045d27b2f3cc2d04b13
Ankit Kumar <147314785+ankit485803@users.noreply.github.com>
2026-06-23 18:06:56 +0530

    Time: 272 ms (75%), Space: 11.9 MB (71.3%) - LeetHub

---
+++ C++/Hard/3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i-06-23-2026-18-06-54.cpp
@@ -0,0 +1,21 @@
+
+// approach2: Bottom-Up DP with Prefix Sums (Optimal)
+// Time: O(n * m), Space: O(m) where m = r - l + 1
+ 
+ class Solution {
+public:
+    static constexpr int MOD = 1000000007;
+    int zigZagArrays(int n, int l, int r) {
+        int m = r - l + 1;
+        vector<int> dp(m, 1);
+
+        for (int i = 2; i <= n; i++) {
+            reverse(dp.begin(), dp.end());
+            int sum = 0;
+            for (auto& d : dp)
+                sum = (sum + exchange(d, sum)) % MOD;
+        }
+
+        return ((accumulate(dp.begin(), dp.end(), 0LL) % MOD) << 1) % MOD;
+    }
+};
